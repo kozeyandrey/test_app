4b4bbf5aaca63c22e21225ac0a68caaec6ed933e
andrewkozey <kozeyandrey@gmail.com>
2017-12-21 13:34:04 +0200

    Updated array format in db/run.rb

--- db/run.rb
+++ db/run.rb
@@ -1,9 +1,9 @@
 require 'pg'
 
-["ex_app", "ex_app_test"].each do |db|
+%w[ex_app ex_app_test].each do |db|
   con = PG::Connection.new(dbname: db, port: 5432)
 
-  sql = File.open('db/schema.sql', 'rb') { |file| file.read }
+  sql = File.open('db/schema.sql', 'rb', &:read)
   con.exec(sql)
 
   con.close
