e587fb036f350fcb70d58e2b663aa24d5015dc45
andrewkozey <kozeyandrey@gmail.com>
2017-12-21 13:20:53 +0200

    Freeze global link

--- scraper.rb
+++ scraper.rb
@@ -3,15 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
-  def self.call
-    # this is how we request the page we're going to scrape
-    uri = URI(
-      'https://sdw.ecb.europa.eu/quickview.do?SERIES_KEY=120.EXR.D.USD.EUR.SP00.A'
-    )
-    page = HTTParty.get(uri.to_s)
+  LINK = URI(
+    'https://sdw.ecb.europa.eu/quickview.do?SERIES_KEY=120.EXR.D.USD.EUR.SP00.A'
+  ).freeze
 
-    # this is where we transform our http response into a nokogiri object
-    # so that we can parse it
+  def self.call
+    page = HTTParty.get(LINK.to_s)
     parse_page = Nokogiri::HTML(page)
 
     links = parse_page.css('a')
@@ -19,6 +16,6 @@ class Scraper
       link.attribute('href').to_s.gsub!('type=csv', 'type=csv')
     end
 
-    open('http://' + uri.host + href.compact.first)
+    open('http://' + LINK.host + href.compact.first)
   end
 end
